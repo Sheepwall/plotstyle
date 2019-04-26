2f2a0ae4e589a91101527d5bd6cb32f95a2bd89d
August Svensson <a.sve@live.se>
2019-04-26 10:12:25 +0200

    First commit.

---
+++ defaultSettings.m
@@ -0,0 +1,7 @@
+% Set default plot settings.
+set(0, 'defaultTextInterpreter', 'default');
+set(0, 'defaultAxesFontSize', 'default');
+set(0, 'DefaultAxesTitleFontSizeMultiplier', 'default');
+set(0, 'DefaultAxesColorOrder', 'default');
+set(0, 'DefaultAxesLineStyleOrder', 'default');
+set(0, 'DefaultLineLineWidth', 'default');
---
+++ userSettings.m
@@ -0,0 +1,7 @@
+% Set plot style settings.
+set(0, 'DefaultTextInterpreter', 'latex');          % Latex for math
+set(0, 'DefaultAxesFontSize', 12);                  % Titles, labels size
+set(0, 'DefaultAxesTitleFontSizeMultiplier', 1.5);  % Titles larger
+set(0, 'DefaultAxesColorOrder', [0 0 0]);           % Default black line
+set(0, 'DefaultAxesLineStyleOrder', '-|--|.-|:');   % Separate by styles
+set(0, 'DefaultLineLineWidth', 2);                  % Thicker lines
